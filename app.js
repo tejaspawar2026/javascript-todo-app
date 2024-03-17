826fdff459e93d9c8e6030dda60d2bce8ad30cb2
Tejas <tejaspawar2026@gmail.com>
2024-03-17 17:15:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const item = document.querySelector("#item")
+const toDoBox = document.querySelector("#to-do-box")
+
+item.addEventListener(
+    "keyup",
+    function(event) {
+        if (event.key == "Enter") {
+            addToDo(this.value)
+            this.value = ""
+        }
+    }
+)
+
+const addToDo = (item) => {
+  if (item == "") {
+    alert("Enter valid input");
+    return false;
+  }
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+         ${item}
+         <i class="bi bi-trash"></i>
+    `;
+
+    listItem.addEventListener(
+        "click",
+        function() {
+            this.classList.toggle("done");
+        }
+    )
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function() {
+            listItem.remove()
+        }
+    )
+    toDoBox.appendChild(listItem)
+}
